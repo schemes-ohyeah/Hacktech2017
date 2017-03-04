d1c75c3c19ea9601c4a03ff62424d2818219282f
mandarker <springnguyen98@gmail.com>
2017-03-04 12:23:56 -0800

    Prettify the json file returned by getTagImage function

--- tester.py
+++ tester.py
@@ -1,13 +1,16 @@
 import scraper
+import json
 
 # getTagImage(), getCelebrity(), getOCR()
 
 def main():
-    links = ["example.com", "example.com/2"]
+    links = ["http://img.wennermedia.com/social/trumpfamily-0a0680e3-ea21-45c5-a9ad-c51fec63dffa.jpg", "https://blogs-images.forbes.com/brianrashid/files/2015/09/NYC-FORBES-1940x970.jpg"]
     for link in links:
         print("This is the tag image data")
         print("===========================")
-        print(scraper.getTagImage(link))
+        #print(scraper.getTagImage(link))
+        print(json.dumps((json.loads(scraper.getTagImage(link).decode('utf-8'))), indent = 4, sort_keys = True))
+
         print("\n\n\n\n")
 
         print("This is the celebrity data")
