e8dc71ac0c8e4d2a237068c13027b6bf5c25e6d4
mduong15 <michael_9630@yahoo.com>
2017-03-04 12:37:37 -0800

    Changed prettify into a function

--- tester.py
+++ tester.py
@@ -3,23 +3,27 @@ import json
 
 # getTagImage(), getCelebrity(), getOCR()
 
+def format_json(js):
+    return json.dumps((json.loads(js.decode('utf-8'))), indent = 4, sort_keys = True)
+
 def main():
     links = ["http://img.wennermedia.com/social/trumpfamily-0a0680e3-ea21-45c5-a9ad-c51fec63dffa.jpg", "https://blogs-images.forbes.com/brianrashid/files/2015/09/NYC-FORBES-1940x970.jpg"]
     for link in links:
         print("This is the tag image data")
         print("===========================")
         #print(scraper.getTagImage(link))
-        print(json.dumps((json.loads(scraper.getTagImage(link).decode('utf-8'))), indent = 4, sort_keys = True))
+        #print(json.dumps((json.loads(scraper.getTagImage(link).decode('utf-8'))), indent = 4, sort_keys = True))
+        print(format_json(scraper.getTagImage(link)))
 
         print("\n\n\n\n")
 
         print("This is the celebrity data")
         print("===========================")
-        print(scraper.getCelebrity(link))
+        print(format_json(scraper.getCelebrity(link)))
         print("\n\n\n\n")
 
         print("This is the OCR data")
         print("===========================")
-        print(scraper.getOCR(link))
+        print(format_json(scraper.getOCR(link)))
 
 main()
