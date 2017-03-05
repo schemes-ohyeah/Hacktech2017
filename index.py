8224df190882b05fcb20aee5fcff0862f5453d50
Kevin Tran <me@kevintran.codes>
2017-03-05 20:39:40 +0000

    .

--- index.py
+++ index.py
@@ -2,13 +2,14 @@ import GraphRunner
 from flask import Flask, render_template
 app = Flask(__name__)
 
+image_graph, word_graph = GraphRunner.get_graphs()
+image_data = GraphRunner.get_weights(image_graph)
+word_data = GraphRunner.get_weights(word_graph)
+
+
 @app.route("/")
 def index():
     # define some loop data
-    image_graph, word_graph = GraphRunner.get_graphs()
-    image_data = GraphRunner.get_weights(image_graph)
-    word_data = GraphRunner.get_weights(word_graph)
-
     return render_template("article.html", image_data = image_data, word_data = word_data)
 
-app.run()
+app.run(host="0.0.0.0", port=80)
