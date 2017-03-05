fb0e07b3c56e7d8ddac0064d8e2bc90bbc5c6aad
mandarker <springnguyen98@gmail.com>
2017-03-05 11:04:40 -0800

    Complete loop on webpage using flask

--- index.py
+++ index.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     # define some loop data
-    image_data, word_data = GraphRunner.get_weights(GraphRunner.get_graphs())
+    image_graph, word_graph = GraphRunner.get_graphs()
+    image_data = GraphRunner.get_weights(image_graph)
+    word_data = GraphRunner.get_weights(word_graph)
 
     return render_template("article.html", image_data = image_data, word_data = word_data)
 
