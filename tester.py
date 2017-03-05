1e1bd1988f451d54764ed973eb2c535f0592972e
mandarker <springnguyen98@gmail.com>
2017-03-04 18:51:40 -0800

    Complete fileio and storage for links/backup links

--- tester.py
+++ tester.py
@@ -3,6 +3,7 @@ import json
 import requests
 import calculate_weight
 from urllib.parse import urlparse
+import time
 
 # getTagImage(), getCelebrity(), getOCR()
 
@@ -28,30 +29,47 @@ def main():
         # print(format_json(scraper.get_ocr(link)))
     """
 
-    subreddits = ["http://www.reddit.com/r/all/top.json?limit=50", "http://www.reddit.com/r/funny/top.json?limit=50", "http://www.reddit.com/r/pics/top.json?limit=50"]
     count = 0
-
+    subreddits = ["http://www.reddit.com/r/all/top.json?limit=50", "http://www.reddit.com/r/funny/top.json?limit=50",
+                  "http://www.reddit.com/r/pics/top.json?limit=50"]
     images = set()
 
-    for subreddit in subreddits:
-        r = requests.get(subreddit)
-        r.text
-        data = r.json()
+    try:
+        file = open("backup_links.txt", "r+")
+    except IOError:
+        print("File not found.")
+    else:
+        for subreddit in subreddits:
+            print("Loading images...")
+            try:
+                req = requests.get(subreddit)
+                req.text
+                data = req.json()
 
-
-        for value in data['data']['children']:
-            if urlparse(value['data']['url'])[2][-4:] == ".jpg" or urlparse(value['data']['url'])[2][-4:] == ".png":
-                # print("Title: ", value['data']['title'])
-                # print("\t", "url: ", value['data']['url'])
-                images.add(value['data']['url'])
+                for value in data['data']['children']:
+                    time.sleep(3)
+                    if urlparse(value['data']['url'])[2][-4:] == ".jpg" or urlparse(value['data']['url'])[2][-4:] == ".png":
+                        images.add(value['data']['url'] + "\n")
+                        # print("Title: ", value['data']['title'])
+                        # print("\t", "url: ", value['data']['url'])
+            except:
+                print("Something occurred. Loading backup links...")
+                images.clear();
+                for url in file:
+                    images.add(url)
+                break;
+            else:
+                pass
 
     for url in images:
-        print(url)
-        count = count + 1
+       print(url)
+       count = count + 1
 
     print(count)
 
-    data_a = scraper.get_tag_image("https://portalstoragewuprod2.azureedge.net/vision/Analysis/1-1.jpg")
-    data_b = scraper.get_tag_image("https://portalstoragewuprod2.azureedge.net/vision/Analysis/7-1.jpg")
-    print(calculate_weight.calculateTagWeight(data_a, data_b))
+    # data_a = scraper.get_tag_image("https://portalstoragewuprod2.azureedge.net/vision/Analysis/1-1.jpg")
+    # data_b = scraper.get_tag_image("https://portalstoragewuprod2.azureedge.net/vision/Analysis/7-1.jpg")
+    # print(calculate_weight.calculateTagWeight(data_a, data_b))
+
+    file.close()
 main()
